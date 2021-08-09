f16d550abe59984751b52a99bc58daec2a9365e6
GRACEWELLTECH <51518581+GRACEWELLTECH@users.noreply.github.com>
2021-08-09 15:39:41 +0530

    Create DistributorMaster.ts

---
+++ src/entity/Purchase/DistributorMaster.ts
@@ -0,0 +1,72 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class DistributorMaster {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    ApprovalRequired: boolean;
+
+    @Column()
+    Name: string;
+
+    @Column()
+    AlternateName: string;
+
+    @Column()
+    Address: string;
+
+    @Column()
+    Pincode: number;
+
+    @Column()
+    Place: string;
+
+    @Column()
+    State: string;
+
+    @Column()
+    Country: string;
+
+    @Column()
+    OfficeLandline: number;
+
+    @Column()
+    SalesPersonName: string;
+
+    @Column()
+    SalesPersonMobile: number;
+
+    @Column()
+    SalesPersonWhatsapp: number;
+
+    @Column()
+    DeliveryPersonName: string;
+
+    @Column()
+    DeliveryPersonMobile: number;
+
+    @Column()
+    DeliveryPersonWhatsapp: number;
+
+    @Column()
+    EmailID: string;
+
+    @Column()
+    InventoryType: string;
+
+    @Column()
+    GSTType: string;
+
+    @Column()
+    Purchase: string;
+
+    @Column()
+    GSTNumber: string;
+
+    @Column()
+    OrderVsDeliveryType: string;
+}
+
