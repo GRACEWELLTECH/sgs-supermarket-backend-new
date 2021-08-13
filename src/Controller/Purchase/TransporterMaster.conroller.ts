25758f4371e4362039fddc5ee52ff93f181df938
GRACEWELLTECH <51518581+GRACEWELLTECH@users.noreply.github.com>
2021-08-13 15:36:01 +0530

    Create TransporterMaster.conroller.ts

---
+++ src/Controller/Purchase/TransporterMaster.conroller.ts
@@ -0,0 +1,39 @@
+import {Request, Response} from 'express';
+import {connection} from "../../connection/Connection";
+import {getRepository} from "typeorm";
+
+import {TransporterMaster} from "../../entity/Purchase/TransporterMaster"
+
+export class TransporterMasterController{
+    async getTransporterMaster(req, res,next) {
+      connection
+                .then(async connection => {
+                    const TransporterMasterList: TransporterMaster[] = await connection.manager.find(TransporterMaster);
+                    res.json(TransporterMasterList);
+                })
+                .catch(error => {
+                    console.error("Error ", error);
+                    res.json(error);
+                }); 
+      
+    }
+    async createTransporterMaster(req, res,next) {
+      connection
+                .then(async connection => {
+                    console.log(req.body)
+                    let Type = new TransporterMaster()
+               
+                    Type.NameTransporter
+                     = req.body.Name
+                    Type.Address = req.body.Address
+                
+                    await connection.manager.save(Type);
+                res.json({message: "Successfully Saved."})
+                })
+                .catch(error => {
+                    console.error("Error ", error);
+                    res.json(error);
+                }); 
+      
+    }
+}
