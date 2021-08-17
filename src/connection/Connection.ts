dcba7b4e3c9357922b731cf1decff5c2a334313b
GRACEWELLTECH <51518581+GRACEWELLTECH@users.noreply.github.com>
2021-08-17 12:07:53 +0530

    Update Connection.ts

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -12,6 +12,20 @@ import SubKind from "../entity/SubKind";
 import {Manufacturer} from "../entity/Manufacturer";
 import {Brand} from "../entity/Brand";
 import {Product} from "../entity/Product";
+<<<<<<< Updated upstream
+=======
+import {Gst} from "../entity/Gst";
+import {ItemVsEan} from "../entity/Inventory/ItemVsEan";
+import {HsnVsGst} from "../entity/Inventory/HsnVsGst";
+import {Distributor} from "../entity/Purchase/Distributer/DistributorMaster";
+import {DistributerDeliveryPersion} from "../entity/Purchase/Distributer/DistributerDeliveryPerson";
+import {DistributerLandline} from "../entity/Purchase/Distributer/DistributerLandline";
+import {DistributerMail} from "../entity/Purchase/Distributer/DistributerMail";
+import {WeightUnit} from "../entity/WeightUnit"
+import {OrderVsDelivery} from "../entity/Purchase/Distributer/OrderVsDelivery";
+import { TransporterMaster } from "../entity/Purchase/TransporterMaster"
+// import {DistributerSalesPersion} from "../entity/Purchase/Distributer/DistributerSalespersion";
+>>>>>>> Stashed changes
 
 
 import {WeightUnit} from "../entity/WeightUnit";
@@ -28,9 +42,20 @@ export const connection = createConnection({
      type: "mysql",
     host: "localhost",
     port: 3306,
+<<<<<<< Updated upstream
     username: "gracewell",
     password: "12345",
     database: "sgs_market",
+=======
+    //username: "abinesh",
+    //password: "Passw0rd",
+    // database: "sgs_db",
+  
+    username: "root",
+    password: "12345",
+   database: "sgs_market",
+ 
+>>>>>>> Stashed changes
     entities: [
         Category,SubCategory,
         SuperHero,Power,
@@ -39,9 +64,27 @@ export const connection = createConnection({
         WeightUnit,
         Manufacturer,
         Brand,
+<<<<<<< Updated upstream
         Product,
 
         assemblykit,changeselling, DistributorMaster
+=======
+        Product,Gst,
+        ManufacyturerVsBrand,
+        //inventoary
+        ItemVsEan,
+        HsnVsGst,
+
+        //purchease 
+        Distributor,
+        DistributerDeliveryPersion,
+        DistributerLandline,
+        DistributerMail,
+        TransporterMaster,
+
+        // DistributerSalesPersion,
+        OrderVsDelivery
+>>>>>>> Stashed changes
     ],
     synchronize: true,
     logging: false
