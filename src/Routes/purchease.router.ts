ddd1adaeed2e34a26b0aba222c410192996019a0
GRACEWELLTECH <51518581+GRACEWELLTECH@users.noreply.github.com>
2021-08-17 09:47:09 +0530

    Update purchease.router.ts

--- src/Routes/purchease.router.ts
+++ src/Routes/purchease.router.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 
 import {DistributorMasterController} from '../Controller/Purchase/DistributorMastor.controller'
+import {TransporterMasterController} from '../Controller/Purchase/TransporterMaster.conroller'
 
 const purcheaseRouter=Router();
-
 const distributer=new DistributorMasterController()
+const transporter=new TransporterMasterController()
 
 purcheaseRouter.post('/createDistributer',distributer.createDistributorMaster)
 purcheaseRouter.get('/getDistributerList',distributer.getDistributorMaster)
 
+purcheaseRouter.post('/createTransporterMaster',transporter.createTransporterMaster)
+purcheaseRouter.get('/getTransporterMasterList',transporter.getTransporterMaster)
+
+
+
 export default purcheaseRouter;
+
