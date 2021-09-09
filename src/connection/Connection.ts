92814747ada13f2d3dd48fe8d5abe66968dbdc87
AbineshRaja <abineshraja169@gmail.com>
2021-09-09 11:01:43 +0530

    Update Connection.ts

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -28,7 +28,7 @@ import {CategoryVsBrand} from "../entity/Purchase/CategoryVsBrand";
 import {RepackEntry} from "../entity/Inventory/RepackEntry";
 import {Repack} from "../entity/Inventory/Repack";
 import {RepackStock} from "../entity/Inventory/repackStock";
-import {RepackTransfer} from "../entity/Inventory/RepackTransfer";
+import {RepackTransfer} from "../entity/Inventory/RePackTransfer";
 import {RepackTransferDetail} from "../entity/Inventory/RepackTransferDetails";
 
 // import {DistributerSalesPersion} from "../entity/Purchase/Distributer/DistributerSalespersion";
