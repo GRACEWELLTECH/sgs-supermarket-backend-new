dc45d42f42f53ff217279b5ba0bec8b3e85286c2
AbineshRaja <abineshraja169@gmail.com>
2021-09-15 12:40:36 +0530

    Update OpeningStockController.ts

--- src/Controller/Inventory/OpeningStockController.ts
+++ src/Controller/Inventory/OpeningStockController.ts
@@ -29,7 +29,7 @@ getProductsForOpeningStock(req,res,next){
 
     connection
     .then(async connection => {
-       let list=await connection.manager.query("select A.*,B.id as stockId,B.quantity from product A left outer join opening_stock B on A.id=b.productId;");
+       let list=await connection.manager.query("select A.*,B.id as stockId,B.quantity from product A left outer join opening_stock B on A.id=B.productId;");
 
        return res.status(200).json({message:"Success",data:list})
     }).catch(err => {
