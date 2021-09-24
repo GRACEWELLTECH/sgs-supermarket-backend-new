4c010354c706cdc64b96e9dc06dda2c1e6e0140f
AbineshRaja <abineshraja169@gmail.com>
2021-09-24 13:23:13 +0530

    cat vs Manufact

--- src/Controller/Purchase/categoryVSManufacturer.controller.ts
+++ src/Controller/Purchase/categoryVSManufacturer.controller.ts
@@ -3,12 +3,12 @@ import {connection} from "../../connection/Connection";
 import {getRepository} from 'typeorm'
 import {CategoryVsManufacturer} from '../../entity/Purchase/CategoryVsManufacturer'
 
-export class CategoryVsBrandController{
+export class CategoryVsManufacturerController{
 
     async asssignCategoryVsManufacturer(req: Request,res: Response,next){
 
         let repo=getRepository(CategoryVsManufacturer);
-        let ManufacturerList=req.body.Manufacturer;
+        let ManufacturerList=req.body.manufacturer;
        let saveArray:CategoryVsManufacturer[]=[]
        ManufacturerList.forEach(Manufacturer => {
             let newObj=new CategoryVsManufacturer
@@ -20,6 +20,7 @@ export class CategoryVsBrandController{
             newObj.productType=req.body.productType;
             newObj.kind=req.body.kind;
             newObj.subKind=req.body.subKind;
+            newObj.manufacturer=Manufacturer.id;
             saveArray.push(newObj);
         })
 
@@ -29,4 +30,35 @@ export class CategoryVsBrandController{
 
     
     }
+
+    getCategoryVsManufacturer(req,res,next){
+
+        let condtion:any = {};
+
+        if(req.body.category!=null&&req.body.category!=""&&req.body.category!=undefined)
+        condtion.category=req.body.category;
+
+        if(req.body.subCategory!=null&&req.body.subCategory!=""&&req.body.subCategory!=undefined)
+        condtion.subCategory=req.body.subCategory;
+
+        if(req.body.type!=null&&req.body.type!=""&&req.body.type!=undefined)
+        condtion.type=req.body.type;
+
+        if(req.body.subType!=null&&req.body.subType!=""&&req.body.subType!=undefined)
+        condtion.subType=req.body.subType;
+
+        if(req.body.kind!=null&&req.body.kind!=""&&req.body.kind!=undefined)
+        condtion.kind=req.body.kind;
+
+        if(req.body.subKind!=null&&req.body.subKind!=""&&req.body.subKind!=undefined)
+        condtion.subKind=req.body.subKind;
+
+        getRepository(CategoryVsManufacturer).find({where:{condtion},relations:["manufacturer"]}).then(list => {
+            return res.status(200).json({data:list});
+        }).catch(err => {
+            return res.status(400).json({Error:err});
+        })
+
+    }
+}
 }
--- src/Routes/purchease.router.ts
+++ src/Routes/purchease.router.ts
@@ -3,18 +3,25 @@ import { Router } from 'express';
 import {DistributorMasterController} from '../Controller/Purchase/DistributorMastor.controller'
 import {TransporterMasterController} from '../Controller/Purchase/TransporterMaster.conroller'
 import {CategoryVsBrandController} from '../Controller/Purchase/categtoryVsBrand.controller'
+import {CategoryVsManufacturerController} from '../Controller/Purchase/categoryVSManufacturer.controller'
 import {DistributerVsManufacturerController} from '../Controller/Purchase/DistributerVsManufacturer.controller'
 
 const purcheaseRouter=Router();
 const distributer=new DistributorMasterController()
 const transporter=new TransporterMasterController()
 const catVsBrand=new CategoryVsBrandController()
+const catVsManu=new CategoryVsManufacturerController()
 const distributerVsManufacturer=new DistributerVsManufacturerController()
 
 purcheaseRouter.post('/createDistributer',distributer.createDistributorMaster)
 purcheaseRouter.get('/getDistributerList',distributer.getDistributorMaster)
+
+// catVsBrand
 purcheaseRouter.post('/assignCategoryVsBrand',catVsBrand.asssignCategoryVsBrand)
 purcheaseRouter.post('/getbrandbycategory',catVsBrand.getbrandVbycategory)
+// catVsManufacturer
+purcheaseRouter.post('/assignCategoryVsManufacturer',catVsManu.asssignCategoryVsManufacturer)
+purcheaseRouter.post('/getManufacturerbycategory',catVsManu.getCategoryVsManufacturer)
 
 purcheaseRouter.post('/createTransporterMaster',transporter.createTransporterMaster)
 purcheaseRouter.get('/getTransporterMasterList',transporter.getTransporterMaster)
